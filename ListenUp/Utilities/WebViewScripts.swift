783b5efdcfd547149ff751c6fb72091b87c3fd32
smh <soeminhtut73@gmail.com>
2025-10-12 13:49:35 +0700

    clear bug in WebViewScript

--- ListenUp/Utilities/WebViewScripts.swift
+++ ListenUp/Utilities/WebViewScripts.swift
@@ -10,18 +10,6 @@ import WebBrowser
 import WebKit
 
 struct WebViewScripts {
-//    static let cssString = """
-//      /* Make video inline and hide header */
-//      video, iframe {
-//        width: 100% !important;
-//        height: auto !important;
-//        display: block !important;
-//        object-fit: contain !important;
-//      }
-//      #masthead-container, #player-ads {
-//        display: none !important;
-//      }
-//    """
     
     static let jsString = """
       // Add playsinline attributes to videos & iframes
@@ -30,68 +18,6 @@ struct WebViewScripts {
         el.setAttribute('webkit-playsinline', '');
       });
     """
-    
-//    static let cssInjection = """
-//      var style = document.createElement('style');
-//      style.innerHTML = `\(cssString)`;
-//      document.head.appendChild(style);
-//    """
-    
-//    static let trackerJS = """
-//        (function(){
-//          function post(name, payload){
-//            try { window.webkit.messageHandlers[name].postMessage(payload); }
-//            catch(e){
-//              try { window.webkit.messageHandlers.mediaEvent.postMessage({type:'pageURL_post_fail', name, error: String(e)}); } catch(_){}
-//            }
-//          }
-//
-//          function notify(reason){
-//            post('pageURL', { href: location.href, reason: reason || 'unknown' });
-//          }
-//
-//          // 0) Post right away
-//          notify('init');
-//
-//          // 1) History API hooks
-//          ['pushState','replaceState'].forEach(function(fn){
-//            var orig = history[fn];
-//            history[fn] = function(){
-//              var ret = orig.apply(this, arguments);
-//              setTimeout(function(){ notify('history:'+fn); }, 0);
-//              return ret;
-//            };
-//          });
-//          window.addEventListener('popstate', function(){ notify('popstate'); }, true);
-//          window.addEventListener('hashchange', function(){ notify('hashchange'); }, true);
-//
-//          // 2) YouTube SPA signals
-//          window.addEventListener('yt-navigate-finish', function(){ notify('yt-navigate-finish'); }, true);
-//          window.addEventListener('yt-page-data-updated', function(){ notify('yt-page-data-updated'); }, true);
-//
-//          // 3) Anchor clicks
-//          document.addEventListener('click', function(e){
-//            var a = e.target && e.target.closest && e.target.closest('a[href]');
-//            if (a) setTimeout(function(){ notify('click'); }, 0);
-//          }, true);
-//
-//          // 4) MutationObserver (title/attrs)
-//          try {
-//            var mo = new MutationObserver(function(){ notify('mo'); });
-//            mo.observe(document.documentElement, {subtree:true, childList:true, attributes:true, attributeFilter:['href','src','title']});
-//          } catch(e) {
-//            post('mediaEvent', {type:'pageURL_mo_error', error:String(e)});
-//          }
-//
-//          // 5) Fallback timer
-//          (function(){
-//            var last = location.href;
-//            setInterval(function(){
-//              if (location.href !== last){ last = location.href; notify('interval'); }
-//            }, 500);
-//          })();
-//        })();
-//        """
     
     static let trackerJS = """
     (function(){
@@ -149,58 +75,6 @@ struct WebViewScripts {
       }, 500);
     })();
     """
-    
-//    static let mediaObserverJS = """
-//    (function() {
-//        // Store original play methods
-//        const originalVideoPlay = HTMLVideoElement.prototype.play;
-//        const originalAudioPlay = HTMLAudioElement.prototype.play;
-//        
-//        // Override video play method
-//        HTMLVideoElement.prototype.play = function() {
-//            const src = this.currentSrc || this.src;
-//            if (src) {
-//                window.webkit.messageHandlers.mediaEvent.postMessage({
-//                    type: 'video',
-//                    action: 'play',
-//                    src: src,
-//                    currentTime: this.currentTime
-//                });
-//            }
-//            return originalVideoPlay.apply(this, arguments);
-//        };
-//        
-//        // Override audio play method
-//        HTMLAudioElement.prototype.play = function() {
-//            const src = this.currentSrc || this.src;
-//            if (src) {
-//                window.webkit.messageHandlers.mediaEvent.postMessage({
-//                    type: 'audio',
-//                    action: 'play',
-//                    src: src,
-//                    currentTime: this.currentTime
-//                });
-//            }
-//            return originalAudioPlay.apply(this, arguments);
-//        };
-//        
-//        // Monitor dynamically added media elements
-//        const observer = new MutationObserver(function(mutations) {
-//            mutations.forEach(function(mutation) {
-//                mutation.addedNodes.forEach(function(node) {
-//                    if (node.tagName === 'VIDEO' || node.tagName === 'AUDIO') {
-//                        console.log('New media element detected:', node.tagName);
-//                    }
-//                });
-//            });
-//        });
-//        
-//        observer.observe(document.body, {
-//            childList: true,
-//            subtree: true
-//        });
-//    })();
-//    """
     
     static let mediaObserverJS = """
     (function () {
@@ -279,13 +153,6 @@ struct WebViewScripts {
       } catch (_) {}
     })();
     """
-
-    
-//    static let cssUserScript = WKUserScript(
-//        source: cssInjection,
-//      injectionTime: .atDocumentStart,
-//      forMainFrameOnly: false
-//    )
 
     // The JS injector
     static let jsUserScript = WKUserScript(
